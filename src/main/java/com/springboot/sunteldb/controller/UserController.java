44f71bc281c2068873b47a0167fffa9714baf535
Abhishek Itagimath <abhishekitagimath24@gmail.com>
2023-02-23 20:39:54 +0530

    Initial commit

---
+++ src/main/java/com/springboot/sunteldb/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.springboot.sunteldb.controller;
+
+import com.springboot.sunteldb.model.User;
+import com.springboot.sunteldb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3001")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+}
---
+++ src/test/java/com/springboot/sunteldb/SunteldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sunteldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunteldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
